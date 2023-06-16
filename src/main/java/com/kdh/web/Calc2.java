36cd3296908750588781bf20f5ae8f1ab48b1576
kwon93 <93umsongaji@gmail.com>
2023-06-16 23:34:49 +0900

    cookie 추가

--- src/main/java/com/kdh/web/Calc2.java
+++ src/main/java/com/kdh/web/Calc2.java
@@ -6,6 +6,7 @@ import java.io.PrintWriter;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -19,6 +20,7 @@ public class Calc2 extends HttpServlet {
 		resp.setContentType("text/html; charset=UTF-8; ");
 		ServletContext app = req.getServletContext(); //application 저장소 
 		HttpSession session =  req.getSession(); //세션이란 현재 접속한 사용자 현재 접속자마다 그 공간이 달라진다.
+		Cookie[] cookies = req.getCookies();
 		
 		PrintWriter out = resp.getWriter();
 		
@@ -32,10 +34,24 @@ public class Calc2 extends HttpServlet {
 		//계산
 		if(op.equals("=")) {
 //			int x = (Integer) app.getAttribute("value1");
-			int x = (Integer) session.getAttribute("value");
+//			int x = (Integer) session.getAttribute("value");
+			int x = 0;
+			for(Cookie c : cookies) {
+				if(c.getName().equals("value")) {
+					x = Integer.parseInt(c.getValue());	
+					break;
+				}
+			}
 			int y = v;
 //			String operator = (String) app.getAttribute("op");
-			String operator = (String) session.getAttribute("op");
+//			String operator = (String) session.getAttribute("op");
+			String operator = "";
+			for(Cookie c : cookies) {
+				if(c.getName().equals("op")) {
+					operator = c.getValue();	
+					break;
+				}
+			}
 			int result = 0;
 			
 			if(operator.equals("+")) {
@@ -48,8 +64,16 @@ public class Calc2 extends HttpServlet {
 		}else {
 //		app.setAttribute("value", v); // map 컬렉션처럼 키 밸류값을 넣는다.
 //		app.setAttribute("op", op);
-		session.setAttribute("value", v);
-		session.setAttribute("op", op);	
+//		session.setAttribute("value", v);
+//		session.setAttribute("op", op);	
+			
+		Cookie valueCookie = new Cookie("value", String.valueOf(v));	
+		Cookie opCookie = new Cookie("op", op);	
+		valueCookie.setPath("/calc2");
+		opCookie.setPath("/calc2"); //특정경로로만 쿠키를 전달할 때 
+		resp.addCookie(valueCookie);
+		resp.addCookie(opCookie);
+		
 		}
 		
 		
